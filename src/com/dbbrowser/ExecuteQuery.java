a75c9ad2fa4559d2c2b451d15216a7bbbcee23da
Jitendra Sonawane <jitendra.sonawane@riaadvisory.com>
2021-02-01 09:12:32 +0530

    Removed unused imports

--- src/com/dbbrowser/ExecuteQuery.java
+++ src/com/dbbrowser/ExecuteQuery.java
@@ -14,8 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.db.Service;
-import com.mysql.jdbc.PreparedStatement;
-import com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException;
 
 /**
  * Servlet implementation class ExecuteQuery
--- src/com/dbbrowser/MetaData.java
+++ src/com/dbbrowser/MetaData.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.db.Service;
-import com.mysql.jdbc.DatabaseMetaData;
 
 /**
  * Servlet implementation class MetaData
